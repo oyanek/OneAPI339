cafbb1c64ce697de8d7edbc2319893e41b66f30c
u189950 <u189950@s001-n065.aidevcloud>
2023-04-27 08:51:32 -0700

    slight changes

--- src/serial/word_cloud_serial.cpp
+++ src/serial/word_cloud_serial.cpp
@@ -17,16 +17,12 @@ string dict = DEF_DICT;
 size_t bits = DEF_BITS;
 
 vector<size_t> hash_words(bits, 0);
-//hash_words.resize(bits, 0);
-//unordered_map<string, size_t> freq;
 
 void hash_count(string word){
     hash<string> h;
     size_t hash = h(word);
     hash_words[hash % bits]++;
 }
-
-// def hash(){} 
 
 int main(const int argc, const char *const argv[]){
     CLI::App app{"Serial word cloud"};
